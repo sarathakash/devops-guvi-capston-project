2eedef80b4feea42741bb09a076f29a2920ecabc
sarath <sarathakash@gmail.com>
2024-06-21 20:38:14 +0530

    this is my seventh commit

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo apt install docker.io -y
 sudo usermod -aG docker ubuntu
 docker build -t ngimg .
 docker run --name nginx-container -d -p 3000:80  ngimg
@@ -8,5 +7,3 @@ docker-compose up -d
 docker-compose build
 docker tag ngimg sarathakashk95/prod:version-1
 docker push sarathakashk95/prod:version-1
-docker tag ngimg sarathakashk95/dev:latest
-docker push sarathakashk95/dev:latest
